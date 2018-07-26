f2aeec873986514383ab530ac21098923f229d89
thinkocapo <thinkocapo@gmail.com>
2018-07-26 23:57:50 +0300

    Multiple Scenes

--- src/Router.js
+++ src/Router.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import { Scene, Router } from 'react-native-router-flux'
 import LoginForm from './components/manager/LoginForm'
+import EmployeeList from './components/manager/EmployeeList'
 
 // need identify 1 parent, root
+// we see Login first because its the first <Scene>
 const RouterComponent = () => {
     return (
         <Router>
             <Scene key="root">
-                <Scene key="login" component={LoginForm} title="Please Login" />
+                <Scene key="login" component={LoginForm} title="Please Login" initial/>
+                <Scene key="employeeList" component={EmployeeList} title="Employees" />
             </Scene>
         </Router>
     )
