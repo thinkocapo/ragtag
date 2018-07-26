42759086f9ed5d931e2f0b30e3b7763902ead00f
thinkocapo <thinkocapo@gmail.com>
2018-07-26 19:39:43 +0300

    update

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -29,7 +29,8 @@ export default (state = INITIAL_STATE, action) => {
         case LOGIN_USER_SUCCESS:
             // * INTERESTING *
             // banana - throw an error here, and it will cause the forebase call to reach .catch, even though the response came back
-            return { ...state, user: action.payload, error: '', loading: false }
+            // return { ...state, user: action.payload, error: '', loading: false, email: '', password: ''} // reset email/password so if user navigate away from app and back, its no longer showing/stored in redux
+            return { ...state, ...INITIAL_STATE, user: action.payload }
         case LOGIN_USER_FAIL:
             return { ...state, error: 'Authentication Failed', loading: false } // could reset pw password: ''
         case LOGIN_USER:
