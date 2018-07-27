3f2fa3504216cb996598a45f0f9ee4905d2e14a0
thinkocapo <thinkocapo@gmail.com>
2018-07-27 18:19:10 +0300

    firebase data

--- src/components/manager/EmployeeCreate.js
+++ src/components/manager/EmployeeCreate.js
@@ -4,6 +4,22 @@ import { Picker, Text } from 'react-native'
 import { employeeUpdate } from '../../actions'
 import { Card, CardSection, InputCustom, ButtonCustom } from '../common'
 
+/*
+service cloud.firestore {
+  match /databases/{database}/documents {
+    match /{document=**} {
+      allow read, write: if false;
+    }
+  }
+}
+
+allow read, update, delete: if request.auth.uid == userId;
+allow create: if request.auth.uid != null;
+should be
+allow read, update, delete: if request.auth.uid == userId;
+allow create: if request.auth.uid != userId;
+?
+*/
 
 class EmployeeCreate extends Component {
     // no style property automatically consumed in CardSection, because CardSection is a component we put to gether. However Picker and react-native components can accept style: flex-direction
