69d55b32db9cf577ecb70e6d75577378e23f7164
thinkocapo <thinkocapo@gmail.com>
2018-07-29 14:52:06 +0300

    works

--- src/components/manager/EmployeeList.js
+++ src/components/manager/EmployeeList.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
-import { View, Text, ListView } from 'react-native'
+import { ListView } from 'react-native'
 import { connect } from 'react-redux'
 import _ from 'lodash'
 import { employeesFetch } from '../../actions'
+import ListItem from './ListItem'
 
 
 class EmployeeList extends Component {
@@ -27,17 +28,23 @@ class EmployeeList extends Component {
     // for when re-naviating back, already have employees in redux
     createDataSource({ employees }) {
         const ds = new ListView.DataSource({
-            rowHasChanged: (RSA_X931_PADDING,r2) => r1 !== r2
+            rowHasChanged: (r1, r2) => r1 !== r2
         })
 
         this.dataSource = ds.cloneWithRows(employees)
     }
 
+    renderRow(employee) {
+        return <ListItem employee={employee} />
+    }
+
     render() {
         return (
-            <View>
-                <Text>Emp List1</Text>
-            </View>
+            <ListView
+                enableEmptySections
+                dataSource={this.dataSource}
+                renderRow={this.renderRow}
+            />
         )
     }
 }
