c9ccbc47254352f24151c907d306263e832e92ec
thinkocapo <thinkocapo@gmail.com>
2018-08-03 20:14:58 +0300

    marker press Id captured

--- src/AppRagTag.js
+++ src/AppRagTag.js
@@ -5,29 +5,30 @@ import { createStore, applyMiddleware } from 'redux'
 import ReduxThunk from 'redux-thunk' // middleware
 import firebase from 'firebase'
 
-import MapView,  { PROVIDER_GOOGLE } from 'react-native-maps';
-
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+import { markers } from './markers'
 
 import reducers from './reducers'
 import { Header } from './components/common'
-// // import LoginForm from './components/manager/LoginForm'
 import Router from './Router'
 
+// 2nd arg {} is for any additional state we want to pass to our redux application. e.g. email/pw flag for our auth reducer. more for server-side rendering
+// 3rd arg is a Store Enhancer (additional functionalities to our store)
+const store = createStore(reducers, {}, applyMiddleware(ReduxThunk))
+
 // https://facebook.github.io/react-native/docs/linking-libraries-ios
-/**
- * 
- * Look at node_modules/react-native-maps/lib/components/MapView and look for AIRMap references, says what to do in iOS setup, XCode, try this
- * and/or
- * See if running on Android works too. run on android first with a <Text> instead of <MapView>, make sure that works... then try the MapView.
- */
-
-// https://github.com/alvelig/react-native-maps-pods-example
-// therealgilles https://github.com/react-community/react-native-maps/issues/789
-
-// big instructions https://gist.github.com/heron2014/e60fa003e9b117ce80d56bb1d5bfe9e0
-// https://github.com/googlemaps/google-maps-ios-utils/blob/master/samples/ObjCDemoApp/ObjCDemoApp/AppDelegate.m
 
 class AppRagTag extends Component {
+
+    state = {
+        markers: markers,
+        region: {
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+        }
+    }
 
     async componentWillMount() {
         // const firebaseInitialized = await firebase.initializeApp({
@@ -41,24 +42,45 @@ class AppRagTag extends Component {
         // console.log('firebase initialized:', firebaseInitialized) //shows config values
 
     }
+
+    onRegionChange(region) {
+        // this.setState({ region }); // keeps re-firing re-focusing
+        // so do nothing...for now...nothing needed
+    }
+    handleOnPress(nativeEvent) {
+        console.log('handleOnPress...', nativeEvent)
+        /*
+            action:"marker-press"
+            coordinate:{longitude: -122.4324, latitude: 37.78825}
+            id:"100"
+            target:2
+        */
+       // Firebase call(sender, receiver)
+    }
+
     render() {
-        console.log('PROVIDER_GOOGLE', PROVIDER_GOOGLE)
-        // 2nd arg {} is for any additional state we want to pass to our redux application. e.g. email/pw flag for our auth reducer. more for server-side rendering
-        // 3rd arg is a Store Enhancer (additional functionalities to our store)
-        const store = createStore(reducers, {}, applyMiddleware(ReduxThunk))
+    
+        // Could make header with menu/buttons for Nav in Rag Tag
         // <Header headerText="Manager Stack App"/>
+
         return (
             <Provider store={store}>
                 <MapView
                     provider={PROVIDER_GOOGLE}
                     style={styles.map}
-                    region={{
-                    latitude: 37.78825,
-                    longitude: -122.4324,
-                    latitudeDelta: 0.015,
-                    longitudeDelta: 0.0121,
-                    }}
+                    region={this.state.region}
+                    onRegionChange={this.onRegionChange.bind(this)}
                 >
+                    {this.state.markers.map((marker, idx) => (
+                        <Marker
+                            identifier={marker.id}
+                            key={idx}
+                            coordinate={marker.latlng}
+                            title={marker.title}
+                            description={marker.description}
+                            onPress={e => this.handleOnPress(e.nativeEvent)}
+                        />
+                    ))}
                 </MapView>
             </Provider>
         )
@@ -66,16 +88,16 @@ class AppRagTag extends Component {
 }
 
 const styles = StyleSheet.create({
- container: {
-   ...StyleSheet.absoluteFillObject,
-   height: 400,
-   width: 400,
-   justifyContent: 'flex-end',
-   alignItems: 'center',
- },
- map: {
-   ...StyleSheet.absoluteFillObject,
- },
-});
+    container: {
+        ...StyleSheet.absoluteFillObject,
+        height: 400,
+        width: 400,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+    },
+    map: {
+        ...StyleSheet.absoluteFillObject,
+    },
+})
 
 export default AppRagTag
---
+++ src/markers.js
@@ -0,0 +1,16 @@
+export const markers = [
+    {
+        latlng: {
+            latitude: 37.78825, // Lafeyette Park (center)
+            longitude: -122.4324
+        },
+        id: "100"
+    },
+    {
+        latlng: {
+            latitude: 37.7599, // MISSION DISTRICT
+            longitude: -122.4148
+        },
+        id: "101"
+    }
+]
