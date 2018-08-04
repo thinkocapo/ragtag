30b9cddb5d697406f0c46fb5acd58cd1cd346a40
thinkocapo <thinkocapo@gmail.com>
2018-08-04 18:40:06 +0300

    rest run basic android app

--- src/AppRagTag.js
+++ src/AppRagTag.js
@@ -10,7 +10,7 @@ import {
 import reducers from './reducers'
 import { Header, SpinnerCustom } from './components/common'
 import Router from './Router'
-import Map from './components/ragtag/map'
+// import Map from './components/ragtag/map'
 
 const store = createStore(reducers, {}, applyMiddleware(ReduxThunk))
 
@@ -20,15 +20,15 @@ class AppRagTag extends Component {
 
     async componentWillMount() {
 
-        const firebaseInitialized = await firebase.initializeApp({
-            apiKey: RAGTAG_API_KEY,
-            authDomain: RAGTAG_AUTH_DOMAIN,
-            databaseURL: RAGTAG_DATABASE_URL,
-            projectId: RAGTAG_PROJECT_ID,
-            storageBucket: RAGTAG_STORAGE_BUCKET,
-            messagingSenderId: RAGTAG_MESSAGING_SENDER_ID
-        }) 
-        console.log('firebase initialized:::', firebaseInitialized)
+        // const firebaseInitialized = await firebase.initializeApp({
+        //     apiKey: RAGTAG_API_KEY,
+        //     authDomain: RAGTAG_AUTH_DOMAIN,
+        //     databaseURL: RAGTAG_DATABASE_URL,
+        //     projectId: RAGTAG_PROJECT_ID,
+        //     storageBucket: RAGTAG_STORAGE_BUCKET,
+        //     messagingSenderId: RAGTAG_MESSAGING_SENDER_ID
+        // }) 
+        // console.log('firebase initialized:::', firebaseInitialized)
         
     }
 
@@ -38,7 +38,7 @@ class AppRagTag extends Component {
             <Provider store={store}>
                 <View style={{ flex: 1 }}>
                     <Header headerText="RAG TAG"/>
-                    <Map />
+                    {/* <Map /> */}
                 </View>
             </Provider>
         )
